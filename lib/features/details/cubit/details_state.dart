0f3a7cd50b6181c7f128f06c8afba59828677c71
Patrycja Wazner <patrycjawazner21@gmail.com>
2023-08-23 17:46:08 +0100

    Add flutterfire_ui

--- lib/features/details/cubit/details_state.dart
+++ lib/features/details/cubit/details_state.dart
@@ -2,6 +2,6 @@ part of 'details_cubit.dart';
 
 @immutable
 class DetailsState {
-  DetailsState({required this.itemModel});
+  const DetailsState({required this.itemModel});
   final ItemModel? itemModel;
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,13 @@
-import 'package:cantwait28/features/home/pages/home_page.dart';
+import 'package:cantwait28/features/auth/pages/auth_gate.dart';
 import 'package:cantwait28/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:cantwait28/features/home/pages/home_page.dart';
+import 'package:cantwait28/features/home/pages/home_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.purple,
       ),
-      home: const HomePage(),
+      home: const AuthGate(),
     );
   }
 }
